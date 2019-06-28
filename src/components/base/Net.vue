c47923850536b1aaa27220f14b07f148b1b25a44
zengyuhan503 <13458685625@163.com>
2019-06-28 18:22:10 +0800

    解析基站配置

--- src/components/base/Net.vue
+++ src/components/base/Net.vue
@@ -5,28 +5,28 @@
         label-width="100px"
         style="padding-right: 20%"
     >
-        <el-form-item label="基站IP">
+        <el-form-item label="基站IP" required>
             <ip-input v-model="form.baseIp"></ip-input>
         </el-form-item>
-        <el-form-item label="基站MASK">
+        <el-form-item label="基站MASK" required>
             <ip-input v-model="form.mask"></ip-input>
         </el-form-item>
-        <el-form-item label="基站MAC">
+        <el-form-item label="基站MAC" required>
             <ip-input v-model="form.mac" :length="6"></ip-input>
         </el-form-item>
-        <el-form-item label="基站端口">
+        <el-form-item label="基站端口" required>
             <el-input v-model="form.basePort"></el-input>
         </el-form-item>
-        <el-form-item label="服务器IP">
+        <el-form-item label="服务器IP" required>
             <ip-input v-model="form.serverIp"></ip-input>
         </el-form-item>
-        <el-form-item label="服务器端口">
+        <el-form-item label="服务器端口" required>
             <el-input v-model="form.serverPort"></el-input>
         </el-form-item>
         <el-form-item label="传输模式">
             <el-radio-group v-model="form.mode">
-                <el-radio label="AA">TCP</el-radio>
-                <el-radio label="55">UDP</el-radio>
+                <el-radio :label="85">UDP</el-radio>
+                <el-radio :label="170">TCP</el-radio>
             </el-radio-group>
         </el-form-item>
         <el-form-item>
@@ -51,11 +51,76 @@ export default class Net extends Vue {
         baseIp: [],
         serverIp: [],
         mask: [],
-        mac: []
+        mac: [],
+        mode: 85
     };
 
     public onSubmit() {
         //
+    }
+
+    // 解析基站网络设置
+    private parse(value: string | IJson) {
+        // [[key, byte, 是否用数组表示]]
+        const keys = [
+            ['baseIp', 4, true],
+            ['mask', 4, true],
+            ['mac', 6, true],
+            ['basePort', 2, false],
+            ['serverIp', 4, true],
+            ['serverPort', 2, false],
+            ['mode', 1, false]
+        ];
+
+        if (typeof value === 'string') {
+            return this.str2obj(keys, value);
+        }
+
+        return this.obj2str(keys, value);
+    }
+
+    // 配置命令串转显示用对象
+    private str2obj(keys: any[], value: string) {
+        const parse210 = (v: string) => +('0x' + v);
+        const parseToArr = (s: number, l: number) => {
+            const arr = value.substr(s, l).match(/\d{2}/g) || [];
+
+            return arr.map(parse210);
+        };
+
+        const data: IJson = {};
+        let start: number = 0;
+        keys.forEach(([k, byte, isArr]) => {
+            const len = <number>byte * 2;
+
+            data[<string>k] = isArr
+                ? parseToArr(start, len)
+                : parse210(value.substr(start, len));
+
+            start += len;
+        });
+
+        return data;
+    }
+
+    // 显示对象转配置命令串
+    private obj2str(keys: any[], value: IJson) {
+        return keys
+            .reduce((a, [k, byte]) => {
+                let str: number | string | number[] = value[k];
+                if (Array.isArray(str)) {
+                    str = str
+                        .map(v => v.toString(16).padStart(2, '0'))
+                        .join('');
+                } else {
+                    str = (+str).toString(16).padStart(byte * 2, '0');
+                }
+
+                a.push(str);
+
+                return a;
+            }, [])
+            .join('');
     }
 }
 </script>
--- src/components/base/Primary.vue
+++ src/components/base/Primary.vue
@@ -5,33 +5,33 @@
         label-width="100px"
         style="padding-right: 20%"
     >
-        <el-form-item label="所属组号">
+        <el-form-item label="所属组号" required prop="groupCode">
             <el-input v-model="form.groupCode"></el-input>
         </el-form-item>
-        <el-form-item label="基站标示">
+        <el-form-item label="基站类型">
             <el-radio-group v-model="form.main">
-                <el-radio :label="0">从基站</el-radio>
-                <el-radio :label="1">主基站</el-radio>
+                <el-radio :label="85">从基站</el-radio>
+                <el-radio :label="170">主基站</el-radio>
             </el-radio-group>
         </el-form-item>
-        <el-form-item label="距离参数">
-            <el-input v-model="form.distance">
+        <el-form-item label="基站模式">
+            <el-radio-group v-model="form.mode">
+                <el-radio :label="17">单基站</el-radio>
+                <el-radio :label="34">下行模式</el-radio>
+                <el-radio :label="51">上行模式</el-radio>
+            </el-radio-group>
+        </el-form-item>
+        <el-form-item
+            label="距离参数"
+            required
+            prop="distance"
+            :rules="{ type: 'number' }"
+        >
+            <el-input v-model.number="form.distance">
                 <template slot="append">
                     cm
                 </template>
             </el-input>
-        </el-form-item>
-        <el-form-item label="时间参数">
-            <el-input v-model="form.time"></el-input>
-        </el-form-item>
-        <el-form-item label="功率等级">
-            <el-input-number
-                v-model="form.power"
-                :step-strictly="true"
-                :step="1"
-                :min="1"
-                :max="5"
-            ></el-input-number>
         </el-form-item>
         <el-form-item label="基站信道">
             <el-radio-group v-model="form.channel">
@@ -39,11 +39,30 @@
                 <el-radio :label="5"></el-radio>
             </el-radio-group>
         </el-form-item>
+        <el-form-item
+            label="时间补偿"
+            required
+            prop="time"
+            :rules="{ type: 'number' }"
+        >
+            <el-input
+                v-model.number="form.time"
+                prefix-icon="el-icon-timer"
+            ></el-input>
+        </el-form-item>
         <el-form-item label="频率等级">
-            <el-radio-group v-model="form.frequency">
-                <el-radio :label="1"></el-radio>
-                <el-radio :label="5"></el-radio>
-            </el-radio-group>
+            <el-rate v-model="form.power" :colors="colors" show-score></el-rate>
+        </el-form-item>
+        <el-form-item label="发送等级">
+            <el-rate
+                v-model="form.send"
+                :colors="colors"
+                :max="3"
+                :low-threshold="1"
+                :high-threshold="3"
+                show-score
+            >
+            </el-rate>
         </el-form-item>
         <el-form-item>
             <el-button type="success" @click="onSubmit">设置</el-button>
@@ -55,34 +74,78 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
+import { ElForm } from 'element-ui/types/form';
 
 @Component
 export default class Primary extends Vue {
     @Prop() public data!: IBaseStation;
 
-    public form: any = {
+    public readonly colors: string[] = ['#99A9BF', '#F7BA2A', '#FF9900'];
+    public form: IJson = {
         power: 1,
-        main: 0,
+        main: 85,
         channel: 2,
-        frequency: 1
+        send: 1,
+        mode: 17
     };
 
     public created() {
-        this.$http
-            .post({
-                url: '/api/protocol/sendProtocol',
-                body: {
-                    ip: this.data.ip,
-                    port: 50000,
-                    protocol: '2345201801230D0A'
-                }
-            })
-            .then(console.log)
-            .catch(console.log);
+        // this.$http
+        //     .post({
+        //         url: '/api/protocol/sendProtocol',
+        //         body: {
+        //             ip: this.data.ip,
+        //             port: 50000,
+        //             protocol: '2345201801230D0A'
+        //         }
+        //     })
+        //     .then(console.log)
+        //     .catch(console.log);
     }
 
     public onSubmit() {
-        //
+        const form = <ElForm>this.$refs.form;
+
+        form.validate()
+            .then(() => {
+                console.log(this.parse(this.form));
+            })
+            .catch(console.log);
+    }
+
+    // 解析基站基本配置
+    private parse(value: string | IJson) {
+        // [[key, byte]]
+        const keys = [
+            ['groupCode', 2],
+            ['main', 1],
+            ['mode', 1],
+            ['distance', 2],
+            ['channel', 1],
+            ['time', 4],
+            ['power', 1],
+            ['send', 1]
+        ];
+
+        if (typeof value === 'string') {
+            const data: IJson = {};
+
+            let start: number = 0;
+            keys.forEach(([k, byte]) => {
+                data[k] = +('0x' + value.substr(start, <number>byte * 2));
+                start += <number>byte * 2;
+            });
+
+            return data;
+        }
+
+        return keys
+            .reduce((a, [k, byte]) => {
+                a.push(value[k].toString(16).padStart(<number>byte * 2, '0'));
+
+                return a;
+            }, [])
+            .join('');
     }
 }
 </script>
