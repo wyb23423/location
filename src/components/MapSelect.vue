9d7f9c032f18c29c04732b6a92fb43c409ba70f8
wyb <1354162430@163.com>
2019-09-23 17:39:01 +0800

    ...

--- src/components/MapSelect.vue
+++ src/components/MapSelect.vue
@@ -45,46 +45,46 @@ export default class MapSelect extends Vue {
     }
 
     private async _getMapData() {
-        // try {
-        //     const res = await this.$http.get('/api/map/getall', {
-        //         currentPage: 1,
-        //         pageSize: 100000
-        //     });
-        //     this.options = res.pagedData.datas;
-        // } catch (e) {
-        //     if (e.status !== 404) {
-        //         return;
-        //     }
-
-        this.options = [
-            {
-                id: 1,
-                groupCode: ['0001', '0002'],
-                name: '办公室',
-                filepath: '/image/huijinguangchang.fmap',
-                margin: [
-                    [11582810.7716, 3575751.7814],
-                    [11582810.7716, 3575775.267],
-                    [11582841.6422, 3575775.267],
-                    [11582841.6422, 3575751.7814],
-                    [3073, 2326]
-                ]
-            },
-            {
-                id: 2,
-                name: '办公室-pixi',
-                groupCode: ['0001', '0002'],
-                filepath: '/images/5ad8909aeff13.png',
-                margin: [
-                    [-275, 47],
-                    [-275, 2624],
-                    [3742, 2624],
-                    [3742, 47],
-                    [3073, 2326]
-                ]
+        try {
+            const res = await this.$http.get('/api/map/getall', {
+                currentPage: 1,
+                pageSize: 100000
+            });
+            this.options = res.pagedData.datas;
+        } catch (e) {
+            if (e.status !== 404) {
+                return;
             }
-        ];
-        // }
+
+            this.options = [
+                {
+                    id: 1,
+                    baseGroupList: ['0001', '0002'],
+                    name: '办公室',
+                    filepath: '/image/huijinguangchang.fmap',
+                    margin: [
+                        [11582810.7716, 3575751.7814],
+                        [11582810.7716, 3575775.267],
+                        [11582841.6422, 3575775.267],
+                        [11582841.6422, 3575751.7814],
+                        [3073, 2326]
+                    ]
+                },
+                {
+                    id: 2,
+                    name: '办公室-pixi',
+                    baseGroupList: ['0001', '0002'],
+                    filepath: '/images/5ad8909aeff13.png',
+                    margin: [
+                        [-275, 47],
+                        [-275, 2624],
+                        [3742, 2624],
+                        [3742, 47],
+                        [3073, 2326]
+                    ]
+                }
+            ];
+        }
 
         if (this.options.length) {
             this.value = this.options[0].id;
