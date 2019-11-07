139fd98904bbeb3b9e20b86fcfef2ae48e128eaf
wyb <1354162430@163.com>
2019-11-07 14:15:10 +0800

    标签参数配置界面

--- src/components/edit/TagConfig.vue
+++ src/components/edit/TagConfig.vue
@@ -1,7 +1,8 @@
 <template>
-    <el-form :model="form">
+    <el-form :model="form" style="padding: 20px 5%">
         <el-form-item prop="heartRate" label="测试心率">
             <el-select v-model="form.heartRate">
+                <el-option :value="0x70" label="开始"></el-option>
                 <el-option :value="0x80" label="停止"></el-option>
                 <el-option :value="0x90" label="1s/次"></el-option>
                 <el-option :value="0xa0" label="5s/次"></el-option>
@@ -13,7 +14,7 @@
                     v-for="v of [1, 2, 3, 5]"
                     :key="v"
                     :value="+('0x' + v)"
-                    :label="'信道' + i"
+                    :label="'信道' + v"
                 >
                 </el-option>
             </el-select>
@@ -29,15 +30,16 @@
  * 通过协议配置标签
  */
 import Vue from 'vue';
-import { Prop } from 'vue-property-decorator';
+import { Prop, Component } from 'vue-property-decorator';
 
+@Component
 export default class TagConfig extends Vue {
     private static readonly COMMAND = 0x49;
 
     @Prop({ required: true }) public readonly tagNo!: string;
 
     public form = {
-        heartRate: '70',
+        heartRate: 0x70,
         channel: 1
     };
 
