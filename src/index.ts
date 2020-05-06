cfa26f3ddc248244e348799a82101491229119db
wyb <1354162430@163.com>
2020-05-06 16:04:21 +0800

    feat: 核心功能: 使用ffmpeg推流，以供flv.js使用

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
 import config from "./config.json";
+import * as http from "http";
+import ffmpeg from "fluent-ffmpeg";
+import path from "path";
+import { URL } from "url";
 
-console.log(config);
+// 通过ffmpeg拉流并转码
+function command(url: string): ffmpeg.FfmpegCommand {
+    const ffmpegCommand = ffmpeg(url);
+
+    return ffmpegCommand
+        .setFfmpegPath(path.resolve(config.FFmpegPath, "ffmpeg.exe"))
+        .withNoAudio()
+        .withSize("854x480")
+        .withVideoCodec("libx264")
+        .toFormat("flv")
+        .on("error", (err: Error) => {
+            console.log(err.message);
+            ffmpegCommand.kill("SIGKILL");
+        });
+}
+
+// 从请求地址中获取取流地址
+function parse(req: http.IncomingMessage): string | null {
+    const url = new URL("http://127.0.0.1" + req.url);
+    if (url.pathname === config.pathname) {
+        return url.searchParams.get("url");
+    }
+
+    return null;
+}
+
+http.createServer((req, res) => {
+    const url = parse(req);
+    if (!url) {
+        res.statusCode = 404;
+        res.end(`Not Found: ${req.method} ${req.url}`);
+    } else {
+        command(url).writeToStream(res, { end: true });
+    }
+}).listen(config.port);
