b756074d0915242a66fb7daf1fe97f0f8560c976
wyb <1354162430@163.com>
2020-06-10 16:59:56 +0800

    refactor: 💡 使用koa2创建app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import config from "./config.json";
-import * as http from "http";
 import ffmpeg from "fluent-ffmpeg";
+import Koa from "koa";
+import Router from "koa-router";
 import path from "path";
-import { URL } from "url";
 
 // 通过ffmpeg拉流并转码
 function command(url: string): ffmpeg.FfmpegCommand {
@@ -11,31 +11,18 @@ function command(url: string): ffmpeg.FfmpegCommand {
     return ffmpegCommand
         .setFfmpegPath(path.resolve(config.FFmpegPath, "ffmpeg.exe"))
         .withNoAudio()
-        .withSize("854x480")
+        .withSize("?x480")
         .withVideoCodec("libx264")
         .toFormat("flv")
         .on("error", (err: Error) => {
             console.log(err.message);
-            ffmpegCommand.kill("SIGKILL");
+            ffmpegCommand?.kill("SIGKILL");
         });
 }
 
-// 从请求地址中获取取流地址
-function parse(req: http.IncomingMessage): string | null {
-    const url = new URL("http://127.0.0.1" + req.url);
-    if (url.pathname === config.pathname) {
-        return url.searchParams.get("url");
-    }
+const router = new Router().get("/videoapi", ctx => {
+    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.body = command(ctx.query.url).pipe();
+});
 
-    return null;
-}
-
-http.createServer((req, res) => {
-    const url = parse(req);
-    if (!url) {
-        res.statusCode = 404;
-        res.end(`Not Found: ${req.method} ${req.url}`);
-    } else {
-        command(url).writeToStream(res, { end: true });
-    }
-}).listen(config.port);
+new Koa().use(router.routes()).listen(config.port);
